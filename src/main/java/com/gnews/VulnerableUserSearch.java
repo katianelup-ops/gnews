808bacfdc092a523d3ad8e4ccde2491d1e8421ca
katianelup-ops <katiane.lup@gmail.com>
2026-02-11 20:34:30 -0300

    comiit 3
    Comment out the import statement for Statement class.

--- src/main/java/com/gnews/VulnerableUserSearch.java
+++ src/main/java/com/gnews/VulnerableUserSearch.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+/**import java.sql.Statement;*/
 
 /**
  * Exemplo INTENCIONALMENTE vulnerável (para laboratório):
